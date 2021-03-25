cd541d0e78c2f17148474a89ea363bdb4596ca4c
Thana <thanashr@buffalo.edu>
2021-03-25 05:00:40 -0400

    Updated Hackathon projects

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c36f41e7fd5effb02e337e7d673aca42.js"
+  "/portfolio/precache-manifest.7b54f665af61d9db533b27de9522919b.js"
 );
 
 self.addEventListener('message', (event) => {
