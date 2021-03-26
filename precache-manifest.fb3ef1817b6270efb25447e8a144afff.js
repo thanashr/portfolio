623193baa5d5ad7946440c62decc3068b549f5c5
Thana <thanashr@buffalo.edu>
2021-03-25 22:36:52 -0400

    Updates

---
+++ precache-manifest.fb3ef1817b6270efb25447e8a144afff.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7c457999a3bb3007e7d281f4ccecdc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "466f7835e5860d318632",
+    "url": "/portfolio/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "7f80ce10b98dd35c301c",
+    "url": "/portfolio/static/css/main.e4c6c938.chunk.css"
+  },
+  {
+    "revision": "466f7835e5860d318632",
+    "url": "/portfolio/static/js/2.bfbe1aa5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.bfbe1aa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f80ce10b98dd35c301c",
+    "url": "/portfolio/static/js/main.13ea4f57.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "9f5c6852ed99c13f5749c81269706117",
+    "url": "/portfolio/static/media/accenture_tech.9f5c6852.jpg"
+  },
+  {
+    "revision": "db94bff8cdc16b46b8966bcb9ec33178",
+    "url": "/portfolio/static/media/amrita.db94bff8.png"
+  },
+  {
+    "revision": "af293499343cb005983d6f3bce5c5fe9",
+    "url": "/portfolio/static/media/cover4.af293499.jpg"
+  },
+  {
+    "revision": "5fa2cc265061ee6df4fc95f64a69e525",
+    "url": "/portfolio/static/media/nss.5fa2cc26.png"
+  },
+  {
+    "revision": "0dae34849e1e338acd06601274c5e8aa",
+    "url": "/portfolio/static/media/profile.0dae3484.JPG"
+  },
+  {
+    "revision": "0016e17381b8f53b9cc2b7e837063c2a",
+    "url": "/portfolio/static/media/uandi.0016e173.jpeg"
+  }
+]);
